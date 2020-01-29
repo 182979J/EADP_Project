e8597262bd6330de4b0bd7c7477fcc9a82d8b093
JiaEn182445u <44602073+JiaEn182445u@users.noreply.github.com>
2020-01-29 11:52:44 +0800

    Add files via upload

---
+++ App_Data/dbo.JE_TDTGInfo.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[JE_TDTGInfo] (
+    [Id]            VARCHAR (100)  NOT NULL,
+    [TGName]        VARCHAR (60)   NULL,
+    [TGFirstName]   VARCHAR (30)   NULL,
+    [TGLastName]    VARCHAR (30)  NULL,
+    [TGMobile]      INT            NULL,
+    [TGEmail]       VARCHAR (100)  NULL,
+	[TGProfilePic]  NVARCHAR(MAX)          NULL,
+    [Nric_front]    NVARCHAR(MAX)         NULL,
+    [Nric_back]     NVARCHAR(MAX)           NULL,
+    [License_front] NVARCHAR(MAX)           NULL,
+    [License_back]  NVARCHAR(MAX)           NULL,
+    [ACRA1]         NVARCHAR(MAX)           NULL,
+    [ACRA2]         NVARCHAR(MAX)           NULL,
+    [ACRA3]         NVARCHAR(MAX)           NULL,
+    [Type_of_bank]  NCHAR (20)    NULL,
+    [Bank_num]      INT           NULL,
+	
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
